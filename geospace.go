368138e3dd54091d5fc93c07d104c43a6c7eb376
jdejesus007 <jdejesu3@gmail.com>
2020-02-07 14:07:16 -0500

    Added polygon intersection method true/false

--- geospace.go
+++ geospace.go
@@ -14,6 +14,45 @@ import (
 	"github.com/pkg/errors"
 )
 
+// DoPolygonsIntersect takes two arrays of coordinates and return true/false and
+// error if polygons intersect
+func DoPolygonsIntersect(coordinatesA, coordinatesB []*point.Point) (intersects bool, err error) {
+	// Catch internal C library panics
+	defer func() {
+		if e := recover(); e != nil {
+			var ok bool
+			intersects = false
+			err, ok = e.(error)
+			if !ok {
+				err = errors.Wrap(fmt.Errorf("Error: %v", e), fmt.Sprintf("Debug Stack: %s", string(debug.Stack())))
+				return
+			}
+			err = errors.Wrap(err, fmt.Sprintf("Debug Stack: %s", string(debug.Stack())))
+		}
+	}()
+
+	dotPolygonA, err := getGeosPolygonFromCoordinates(coordinatesA)
+	if err != nil {
+		return false, err
+	}
+
+	dotPolygonB, err := getGeosPolygonFromCoordinates(coordinatesB)
+	if err != nil {
+		return false, err
+	}
+
+	intersectedPoly := geos.Must(dotPolygonA.Intersection(dotPolygonB))
+
+	// If nonintersecting - return empty to skip area
+	if intersectedPoly.String()[len(intersectedPoly.String())-5:] == "EMPTY" {
+		return false, nil
+	}
+
+	log.Println("Intersection -> ", intersectedPoly.String())
+
+	return true, nil
+}
+
 // GetIntersectedPolygonByPolygonAndCenterPointRadiusHaveriseDisc returns one polygon consisting of
 // an individual intersected polygon and a disc derived of the passed in center
 // point and radius with haversine algorithm
